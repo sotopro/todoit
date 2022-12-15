501904ccf14a6e068c3163d016f7863d5daabf0c
sotopro <dasg.1993@gmail.com>
2022-12-15 15:50:44 -0500

    [setup] - expo init

--- App.js
+++ App.js
@@ -1,27 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
-import { useState } from 'react';
-import { Pressable, StyleSheet, Text, View } from 'react-native';
-import { Checkbox } from './src/components';
+import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
-  const [checked, setChecked] = useState(false)
 
-  const handleCheckboxPress = () => {
-    setChecked(prev => {
-      return !prev
-    })
-  }
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
-      <Pressable onPress={handleCheckboxPress} style={styles.checkbox}>
-        <Checkbox 
-          checked={checked}
-          highlightColor="#4444ff"
-          checkmarkColor="#ffffff"
-          boxOutlineColor="#4444ff"
-        />
-      </Pressable>
       <StatusBar style="auto" />
     </View>
   );
