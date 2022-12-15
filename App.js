66461040f2ade7d7c25c5f6f8d987a84e4078073
sotopro <dasg.1993@gmail.com>
2022-12-15 15:49:29 -0500

    [feature] - expo init & create animated checkbox and implement to test

--- App.js
+++ App.js
@@ -1,10 +1,27 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { Pressable, StyleSheet, Text, View } from 'react-native';
+import { Checkbox } from './src/components';
 
 export default function App() {
+  const [checked, setChecked] = useState(false)
+
+  const handleCheckboxPress = () => {
+    setChecked(prev => {
+      return !prev
+    })
+  }
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+      <Pressable onPress={handleCheckboxPress} style={styles.checkbox}>
+        <Checkbox 
+          checked={checked}
+          highlightColor="#4444ff"
+          checkmarkColor="#ffffff"
+          boxOutlineColor="#4444ff"
+        />
+      </Pressable>
       <StatusBar style="auto" />
     </View>
   );
@@ -17,4 +34,8 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  checkbox: {
+    width: 25,
+    height: 25
+  }
 });
